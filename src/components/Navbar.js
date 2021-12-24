ecaac74966d13cd436311738223e5ff6e1824e54
Sanaa Syed <sanaa.syed@shopify.com>
2021-12-23 20:15:35 -0500

    adding more stuff

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,27 +1,25 @@
-import React from "react"
-import scrollTo from "gatsby-plugin-smoothscroll"
-const Navbar = () => {
-  return (
-    <div className="section">
-      <div className="container">
-        <div className="navbar-wrapper">
-          <div
-            role="button"
-            onClick={() => scrollTo("#home")}
-            className="name"
-            tabIndex={0}
-          >
-            Portfolio.
-          </div>
-          <div className="links-wrapper">
-            <button onClick={() => scrollTo("#work")}>Work</button>
-            <button onClick={() => scrollTo("#about")}>About</button>
-            <button onClick={() => scrollTo("#contact")}>Contact</button>
-          </div>
-        </div>
-      </div>
-    </div>
-  )
-}
+import React from "react";
+import scrollTo from "gatsby-plugin-smoothscroll";
+import logo from "../images/logo.png";
 
-export default Navbar
+const Navbar = () => {
+	return (
+		<div className="section">
+			<div className="container">
+				<div className="navbar-wrapper">
+					<div role="button" onClick={() => scrollTo("#home")} tabIndex={0}>
+						<img src={logo} alt="logo" className="name" />
+					</div>
+					<div className="links-wrapper">
+						<button onClick={() => scrollTo("#work")}>Projects</button>
+						<button onClick={() => scrollTo("#about")}>About</button>
+						<button onClick={() => scrollTo("#skills")}>Skills</button>
+						<button onClick={() => scrollTo("#contact")}>Contact</button>
+					</div>
+				</div>
+			</div>
+		</div>
+	);
+};
+
+export default Navbar;
