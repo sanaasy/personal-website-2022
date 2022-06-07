0b411a6085b84d66a2ecb8a5d34fa394758597e5
Sanaa Syed <sanaa@Sanaas-MacBook-Pro.local>
2022-06-06 23:52:41 -0400

    skills

--- src/components/skills.js
+++ src/components/skills.js
@@ -1,9 +1,16 @@
-import React from "react";
+import React, { useState } from "react";
 import Fade from "react-reveal/Fade";
 import data from "../yourdata";
-import Categories from "./atoms/Categories";
+
+const Categories = [
+  "Programming Languages",
+  "Tools and Frameworks",
+  "Certifications",
+];
 
 const Skills = () => {
+  const [filter, setFilter] = useState("Programming Languages");
+
   return (
     <div className="section" id="skills">
       <div className="container">
@@ -11,38 +18,56 @@ const Skills = () => {
           <Fade bottom cascade>
             <h1>Skills</h1>
           </Fade>
-          <div className="select">
-            <h3>Programming Languages</h3>
+          <div className="button-group">
+            {Categories.map((categoryName) => {
+              return (
+                <button
+                  type="button"
+                  style={{
+                    backgroundColor:
+                      filter === categoryName ? "#c99c80" : "#F7F2ED",
+                    color: filter === categoryName ? "#fff" : "#C99C80",
+                  }}
+                  className="btn active"
+                  onClick={() => setFilter(categoryName)}
+                >
+                  {categoryName}
+                </button>
+              );
+            })}
           </div>
-          <div className="skills-grid">
-            {data.skills.map((skill, index) => (
-              <div className="skill" key={index}>
-                <img src={skill.img} alt="css"></img>
-                <p>{skill.para}</p>
-                {/* <div className="gray">
-									<div style={{ width: skill.percent, backgroundColor: skill.colour }}>{skill.percent}</div>
-								</div> */}
-              </div>
-            ))}
-          </div>
-          {/* <h3>Tools and Frameworks</h3>
-					<div className="skills-grid">
-						{data.tools.map((tool, index) => (
-							<div className="skill" key={index}>
-								<img src={tool.img} alt="css"></img>
-								<p>{tool.para}</p>
-							</div>
-						))}
-					</div>
-					<h3>Other</h3>
-					<div className="skills-grid">
-						{data.other.map((skill, index) => (
-							<div className="skill" key={index}>
-								<img src={skill.img} alt="css"></img>
-								<p>{skill.para}</p>
-							</div>
-						))}
-					</div> */}
+          {filter === "Programming Languages" ? (
+            <div className="skills-grid">
+              {data.skills.map((skill, index) => (
+                <div className="skill" key={index}>
+                  <img src={skill.img} alt="css"></img>
+                  <p>{skill.para}</p>
+                </div>
+              ))}
+            </div>
+          ) : null}
+
+          {filter === "Tools and Frameworks" ? (
+            <div className="skills-grid">
+              {data.tools.map((tool, index) => (
+                <div className="skill" key={index}>
+                  <img src={tool.img} alt="css"></img>
+                  <p>{tool.para}</p>
+                </div>
+              ))}
+            </div>
+          ) : null}
+
+          {filter === "Certifications" ? (
+            <div className="skills-grid">
+              {data.other.map((skill, index) => (
+                <div className="skill" key={index}>
+                  <img src={skill.img} alt="css"></img>
+                  <p>{skill.para}</p>
+                </div>
+              ))}
+            </div>
+          ) : null}
         </div>
       </div>
     </div>
