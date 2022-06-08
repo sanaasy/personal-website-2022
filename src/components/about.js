3e2b615764e43140a4ec59eec44055276efa1eb0
Sanaa Syed <sanaa@Sanaas-MacBook-Pro.local>
2022-06-07 22:00:50 -0400

    about section

--- src/components/about.js
+++ src/components/about.js
@@ -1,33 +1,36 @@
 import React from "react";
 import Fade from "react-reveal/Fade";
 import data from "../yourdata";
+import me from "../images/me.jpg";
 
 const About = () => {
-	return (
-		<div className="section" id="about">
-			<div className="container">
-				<div className="about-section">
-					<div className="content">
-						<Fade bottom cascade>
-							<h1>About Me</h1>
-						</Fade>
-						<p>
-							{data.aboutParaOne}
-							<br></br>
-							<br></br>
-							{data.aboutParaTwo}
-							<br></br>
-							<br></br>
-							{data.aboutParaThree}
-						</p>
-					</div>
-					<div className="image-wrapper">
-						<img src={data.aboutImage} alt="about"></img>
-					</div>
-				</div>
-			</div>
-		</div>
-	);
+  return (
+    <div className="section" id="about">
+      <div className="container">
+        <div className="about-section">
+          <div className="content">
+            <Fade bottom cascade>
+              <h1>About Me</h1>
+            </Fade>
+            <p>
+              {data.aboutParaOne}
+              <br></br>
+              <br></br>
+              {data.aboutParaTwo}
+              <br></br>
+              <br></br>
+              {data.aboutParaThree}
+            </p>
+          </div>
+          <div className="image-wrapper">
+            <div className="wrap">
+              <img src={me} alt="about" />
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default About;
--- src/yourdata.js
+++ src/yourdata.js
@@ -101,7 +101,7 @@ export default {
 
 	// About Secton --------------
 	aboutParaOne:
-		"Hi there! My name is Sanaa. I'm a Dev Degree intern and Production Engineer at Shopify and a junior at Carleton University studying Computer Science and Business. Dev Degree is a four year work-integrated learning program with Shopify. We got through training and four placements throughout the four years to experience the various disciplines and teams that Shopify and the tech industry has to offer.",
+		"Hi there! My name is Sanaa. I'm a Dev Degree intern and Production Engineer at Shopify and a junior at Carleton University studying Computer Science and Business. Dev Degree is a four year work-integrated learning program with Shopify. We go through training and four placements throughout the four years to experience the various disciplines and teams that Shopify and the tech industry has to offer.",
 	aboutParaTwo:
 		"Outside of work and school, I am an active lead in driving social impact and supporting/guiding girls into STEM fields and an advocate for bringing tech to everyone. I do so through my work as the Vice President of the Google Developer Student Club chapter at my university, my coaching in the STEMing UP program (of which I was a participant in back in 2018) and working on a mobile app to bring CommonLits educational resources to help those with limited internet access in Latin America.",
 	aboutParaThree:
