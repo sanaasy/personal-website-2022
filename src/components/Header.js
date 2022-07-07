0e64379299e055d7bf6b0cef5885eddcf646288f
Sanaa Syed <sanaa@Sanaas-MacBook-Pro.local>
2022-07-06 22:09:46 -0400

    speaking section

--- src/components/Header.js
+++ src/components/Header.js
@@ -14,35 +14,9 @@ const Header = () => {
             Hi, my name is {data.name}{" "}
           </h2>
           <p>Coder @ Shopify. Writer. CS '22.</p>
-          {/* <a  target="no_blank" href="/resume.pdf">
-          check out my <strong>resume</strong>
-        </a> */}
-          {/* <a target="no_blank" href="/resume.pdf">
-							check out my <strong>resume</strong>
-						</a> */}
-
-          {/* <div className="background">
-          </div> */}
-
-          {/* <div className="shape1">
-            <img src={b1} alt="shape" />
-          </div>
-          <div className="shape2">
-            <img src={b2} alt="shape" />
-          </div>
-          <div className="shape3">
-            <img src={b3} alt="shape" />
-          </div>
-          <div className="shape4">
-            <img src={b4} alt="shape" />
-          </div>
-          <div className="shape5">
-            <img src={b5} alt="shape" />
-          </div>
-          <div className="shape6">
-            <img src={b6} alt="shape" />
-          </div> */}
-
+          <a target="no_blank" href="/resume.pdf">
+            check out my <strong>resume</strong>
+          </a>
           {/* <Fade bottom>
             <div className="social-icons">
               {data.social.map((socialLink, index) => (
